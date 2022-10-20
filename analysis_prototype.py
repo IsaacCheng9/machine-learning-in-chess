70e9de51fc6a3175ec72d115ae21e0c520c9fa06
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-10-20 01:45:49 +0100

    Add analysis of game using Stockfish

--- analysis_prototype.py
+++ analysis_prototype.py
@@ -1,3 +1,5 @@
+import pprint
+
 import chess
 import chess.engine
 import chess.pgn
@@ -8,3 +10,21 @@ engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
 for _ in range(1):
     game = chess.pgn.read_game(pgn)
     print(game)
+    print()
+    # Get the FEN string of the final board position.
+    while game.next():
+        game = game.next()
+    fen = game.board().fen()
+    print(fen)
+    print()
+
+# Get the final position of the board using the FEN.
+board = chess.Board(fen)
+print("(Capitals indicates white pieces, lowercase indicates black pieces.)")
+print(board)
+print()
+# Evaluate the final board position using Stockfish, limiting the depth to
+# prevent the engine from taking too long.
+info = engine.analyse(board, chess.engine.Limit(depth=20))
+# Pretty print the evaluation info for ease of reading.
+pprint.pprint(info)
