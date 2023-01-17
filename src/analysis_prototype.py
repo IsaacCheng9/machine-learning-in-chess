c0385503499c22af6d4ae9db48da2fbd0f79278e
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-01-17 14:56:25 +0000

    Refactor code to improve maintainability

--- src/analysis_prototype.py
+++ src/analysis_prototype.py
@@ -1,3 +1,6 @@
+"""
+A prototype for analysing chess positions using the Stockfish engine.
+"""
 from pprint import pprint
 
 import chess
@@ -41,24 +44,25 @@ def show_board_state(fen: str) -> None:
 
 
 if __name__ == "__main__":
-    engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
-    stockfish = Stockfish("/opt/homebrew/bin/stockfish")
-    pgn_file = "/Users/isaac/Downloads/lichess_db_standard_rated_2022-08.pgn"
-    pgn = open(pgn_file, encoding="utf-8")
+    STOCKFISH_PATH = "/opt/homebrew/bin/stockfish"
+    PGN_PATH = "/Users/isaac/Downloads/lichess_db_standard_rated_2022-08.pgn"
 
-    for _ in range(1):
-        game = chess.pgn.read_game(pgn)
-        print(game)
-        print()
-        # Get the FEN string of the final board position.
-        while game.next():
-            game = game.next()
-        fen = game.board().fen()
-        print(fen)
-        print()
+    engine = chess.engine.SimpleEngine.popen_uci(STOCKFISH_PATH)
+    stockfish = Stockfish(STOCKFISH_PATH)
 
-    show_board_state(fen)
-    info, top_five_moves = analyse_position(fen)
-    pprint(info)
-    print()
-    pprint(top_five_moves)
+    with open(PGN_PATH, encoding="utf-8") as pgn:
+        for _ in range(1):
+            game = chess.pgn.read_game(pgn)
+            print(game)
+            print()
+            # Get the FEN string of the final board position.
+            while game.next():
+                game = game.next()
+            game_fen = game.board().fen()
+            print(game_fen)
+            print()
+            show_board_state(game_fen)
+            game_info, game_top_five_moves = analyse_position(game_fen)
+            pprint(game_info)
+            print()
+            pprint(game_top_five_moves)
