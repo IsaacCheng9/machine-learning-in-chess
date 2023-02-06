8c964fb0e8ac994e679d968f8b121b2699d42599
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-06 02:35:34 +0000

    Add default argument of 1,000,000 games per file

--- src/split_pgn_file.sh
+++ src/split_pgn_file.sh
@@ -1,4 +1,4 @@
 # Split a PGN file into multiple files:
-# $1: Maximum number of chess games per file
-# $2: Path of PGN file to split
-./pgn-extract/pgn-extract -#$1 $2
+# $1: Path of PGN file to split
+# $2: Maximum number of chess games per file (default 1,000,000)
+./pgn-extract/pgn-extract -#${1:-1000000} $2
