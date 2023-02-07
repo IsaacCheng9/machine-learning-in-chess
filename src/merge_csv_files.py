baa66b6b650247b44649f7423661c80ea618f702
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-07 01:15:02 +0000

    Add Python script to merge CSV files

---
+++ src/merge_csv_files.py
@@ -0,0 +1,49 @@
+"""
+A utility script to merge multiple CSV files into one. This can be useful when
+aggregating the results of performing multiprocessing on a split data set.
+"""
+import csv
+from typing import List
+
+
+def merge_csv_files(output_file_name: str, csv_files: List[str]) -> str:
+    """
+    Merge the multiple CSV files into one.
+
+    Args:
+        output_file_name: The name of the output CSV file.
+        csv_files: The paths to the split CSV files.
+
+    Returns:
+        The path of the combined CSV file.
+    """
+    print(f"Merging {len(csv_files)} CSV files into {output_file_name} ...")
+    with open(output_file_name, "w", encoding="utf-8") as output_file:
+        writer = csv.writer(output_file)
+        for i, split_csv_file in enumerate(csv_files):
+            with open(split_csv_file, encoding="utf-8") as file:
+                reader = csv.reader(file)
+                # Skip the header row if it's not the first CSV file.
+                if i != 1:
+                    next(reader)
+                for row in reader:
+                    writer.writerow(row)
+
+    print(f"Merged CSV files into: {output_file_name}")
+    return output_file_name
+
+
+if __name__ == "__main__":
+    OUTPUT_FILE = ""
+    if not OUTPUT_FILE:
+        OUTPUT_FILE = input("Name of the output file: ")
+    if not OUTPUT_FILE.endswith(".csv"):
+        raise ValueError(f"The output file must be a CSV file: {OUTPUT_FILE}")
+    SPLIT_CSV_FILES = []
+    if not SPLIT_CSV_FILES:
+        SPLIT_CSV_FILES = [
+            str(file)
+            for file in input("List of CSV files to merge (space separated): ").split()
+        ]
+
+    merge_csv_files(OUTPUT_FILE, SPLIT_CSV_FILES)
