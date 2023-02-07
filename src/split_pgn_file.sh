ebbc6f378128a829ea042fc8a6c47a9a3680a98c
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2023-02-07 00:48:51 +0000

    Fix order of arguments to implement default arg

--- src/split_pgn_file.sh
+++ src/split_pgn_file.sh
@@ -1,4 +1,4 @@
 # Split a PGN file into multiple files:
 # $1: Path of PGN file to split
 # $2: Maximum number of chess games per file (default 1,000,000)
-./pgn-extract/pgn-extract -#${1:-1000000} $2
+./pgn-extract/pgn-extract -#${2:-1000000} $1
